38ff155491c43b5b0fd2218960a5f72d4710ae88
Silia Cronauer <silia.maria@web.de>
2022-11-11 12:36:05 +0100

    added connection

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "./actions/db_connect.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "./components/styles.php" ?>
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
